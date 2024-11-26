a7b7a45baadc370d694d9f00c6aabb828d2a056c
Samad Khan <ssamadkhan.cs@gmail.com>
2024-11-26 23:29:33 +0530

    episode 2

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement(
     "div" ,
     { id: "parent" },
     React.createElement( "div",{ id: "child"}, [          // no need for the [] if its just one elment
-        React.createElement("h1" , {} , "I am an h1 tag"),
+        React.createElement("h1" , {} , "This is Namaste React"),
         React.createElement("h2" , {} , "I am an h2 tag"),
     ])
 );
@@ -11,7 +14,7 @@ console.log(parent);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));  //  putting h1 into the DOM/browser
 
-root.render(parent); // converts the "heading" object into an heading tag & place it on the DOM
+root.render(parent); // converts the "parent" object into an heading tag & place it on the DOM
 
 
 
