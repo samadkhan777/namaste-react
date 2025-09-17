690fb32bd63a5a5d77baa2e704aa1313d4f9cc0a
Samad Khan <ssamadkhan.cs@gmail.com>
2025-09-17 22:07:01 +0530

    Checked DOM manipulation

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,51 @@
-import User from "./User";
+import user from "./User";
 import UserClass from "./UserClass";
+import { Component } from "react";
 
-const About = () => {
-    return (
-        <div>
-            <h1>About</h1>
 
-            <User name ={"Samad Khan (function)"} />
+class About extends Component {
 
-            <UserClass name ={"Akshay Saini (class)"} location ={"Dehradun Class"}/>
-        </div>
-    );
-};
+    constructor(props) {
+        super(props);
+
+        console.log("Parent Constructor");
+    }
+
+    componentDidMount() {
+        console.log("Parent Component did Mount");
+    }
+
+    render () {
+        console.log("Parent Render");
+        return (
+            <div>
+                <h1>About Class Component</h1>
+                <h2>This is Namaste react</h2>
+
+                <UserClass name={"First"} location ={"Dehradun Class"} />
+                <UserClass name={"Second"} location ={"US"} />
+            </div>
+        );
+    }
+}
+
+/*
+ -Parent Constructor
+ -Parent Render
+
+    -FirstChild Constructor
+    -FirstChild Render
+
+    -SecondChild Constructor
+    -SecondChild Render
+
+    <DOM UPDATED - IN SINGLE BATCH>
+
+    -FirstChild ComponentDidMount
+    -SecondChild ComponentDidMount
+
+-Parent ComponentDidMount
+
+*/
 
 export default About;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -3,15 +3,39 @@ import React from "react";
 class UserClass extends React.Component {
     constructor(props) {
         super(props);
-    
-        console.log(props);
-    }
+
+            this.state = {
+                count: 0,
+            };
+            console.log( this.props.name + "Child Constructor");
+        }
+
+        componentDidMount() {
+            console.log(this.props.name + "Child Component Did Mount");
+
+            //API call
+        }
 
     render() {
+        const { name , location } = this.props;
+        const { count } = this.state;
+
+        console.log(this.props.name +"Child Render");
+
         return (
             <div className="user-card">
-                <h2>Name: {this.props.name}</h2>
-                <h3>Location: {this.props.location}</h3>
+                <h1>Count : {count}</h1>
+                <button             
+                    onClick={() => {
+                        this.setState({       // We can update mutiple variable with one use of "this.setState" 
+                            count: this.state.count + 1,
+                        });
+                    }} 
+                >
+                    Count Increase
+                </button>        
+                <h2>Name: {name}</h2>
+                <h3>Location: {location}</h3>
                 <h4>Contact: @ssamadkhancs7</h4>
             </div>
         );
